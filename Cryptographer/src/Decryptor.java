c0095a67b3ac61a86a826e0759c2e9144dec1120
mrmikemcguire <mcguire@mullenhigh.com>
2014-04-05 15:46:47 -0700

    Initial commit of Encryptor only

---
+++ Cryptographer/src/Decryptor.java
@@ -0,0 +1,25 @@
+
+public class Decryptor
+	{
+
+	public static void main(String[] args)
+		{
+		// strip spaces from 5-character blocks
+		// replace original letters
+		// reverse to original direction
+			// StringBuffer reversedText = new StringBuffer(originalText);
+			// reversedText = reversedText.reverse();
+		// convert to lower case
+					// originalSentence = originalSentence.toLowerCase();
+		// record index locations of spaces
+		// remove spaces
+			// originalText = originalText.replace(" ","");
+			// System.out.println("Your text without spaces is " + originalText);
+		// reverse 
+
+		// replace letters
+		// break up into five-character blocks
+
+		}
+
+	}
---
+++ Cryptographer/src/Encryptor.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Encryptor
+	{
+	static String originalSentence;
+	public static void main(String[] args)
+		{
+		System.out.println("Please enter your sentence to be encrypted: ");
+		Scanner userInput = new Scanner(System.in);
+		originalSentence = userInput.nextLine();
+		userInput.close();
+		originalSentence = originalSentence.toLowerCase();
+		
+		ArrayList locationsOfSpaces = new ArrayList();
+		for (int i = 0; i < originalSentence.length(); i++)
+			{
+			if (originalSentence.substring(i, i + 1).equals(" "))
+				{
+				locationsOfSpaces.add(i);
+				}
+			}
+		
+		System.out.println("Spaces are located at indices");
+		for (int i = 0; i < locationsOfSpaces.size(); i++)
+			{
+			System.out.println(locationsOfSpaces.get(i));
+			}
+			
+		originalSentence = originalSentence.replace(" ","");
+		System.out.println("Your text without spaces is " + 
+		originalSentence);
+	
+		StringBuffer reversedText = new StringBuffer(originalSentence);
+		reversedText = reversedText.reverse();
+		System.out.println("Your reversed text is " + reversedText);
+		
+		for (int i = 0; i < reversedText.length(); i++)
+			{
+			switch (reversedText.substring(i,i +1))
+			{
+			case "a":
+				{
+				reversedText.replace(i, i +1, "xx");
+				break;
+				}
+			case "e":
+				{
+				reversedText.replace(i, i +1, "vv");
+				break;
+				}
+			case "i":
+				{
+				reversedText.replace(i, i +1, "kk");
+				break;
+				}
+			case "o":
+				{
+				reversedText.replace(i, i +1, "qq");
+				break;
+				}
+			case "u":
+				{
+				reversedText.replace(i, i +1, "jj");
+				break;
+				}
+			}
+		}
+		System.out.println("The substituted text now reads " + reversedText);
+	
+		StringBuilder separator = new StringBuilder(reversedText);
+
+		for(int i = 0; i < reversedText.length() / 5; i++) 
+			{
+		    separator.insert(((i + 1) * 5) + i, " ");
+			}
+		String textInBlocks = separator.toString();
+		
+		System.out.println("The final output is " + textInBlocks);
+
+		}
+	}
